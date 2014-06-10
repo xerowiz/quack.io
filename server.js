7482318957a4b1ec86e58ecfda3ae2257c61b244
Julien Levesy <jlevesy@gmail.com>
2014-06-10 08:28:36 +0200

    Init: Added dumb POST /message method

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 var express = require('express'),
         app = express(),
-        http = require('http').createServer(app);
+        bodyParser = require('body-parser'),
+        http = require('http').createServer(app),
+        _ = require('underscore');
 
+/**
+ * Server setup
+ */
 app.set('ipaddr', process.env.SERVERADDR);
 app.set('port', process.env.SERVERPORT);
 
+app.use(bodyParser());
+
+/**
+ * Server API
+ */
 app.get('/', function(request, response) {
-    response.send('Server Up and Running');
+  response.send(200, {message: 'Express is cool'});
+});
+
+app.post('/message', function(req, response) {
+  var message = req.body.message;
+  if(_.isUndefined(message) || _.isEmpty(message.trim())) {
+    return response.json(400, {error: 'Message invalid'}); 
+  }
+
+  return response.json(200, {message: 'Message Received'});
 });
 
 http.listen(app.get('port'), app.get('ipaddr'), function(){
-  console.log('Server up and running, go to :', app.get('ipaddr'), ':', app.get('port'));
+  console.log('Server up and running, go to', app.get('ipaddr')+':'+app.get('port'));
 });
