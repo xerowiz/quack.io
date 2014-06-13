9201bc278d91a5c0b01ef733a9a3bb05f511b0a6
Julien Levesy <jlevesy@gmail.com>
2014-06-13 08:36:08 +0200

    Modules, began to implement user and usermodule

--- test.js
+++ test.js
@@ -1,32 +1,71 @@
+'use strict';
 var expect = require('chai').expect;
 var User = require('./lib/model/User.js');
+var UserModule = require('./lib/modules/usermodule.js');
 var injectr = require('injectr');
 
+var mockio = {
+
+  emitCalled: false,
+
+  sockets: {
+    emit: function(code, data) {
+      this.emitCalled = true;
+    }
+  }
+};
+
+describe('user', function() {
+  describe('#isValid', function() {
+    it('should flag as valid user a valid user', function() {
+      // given
+      var validUser = new User(0, 12, 'robert', 'on');
+      // when
+      var result = validUser.isValid();
+      // then
+      expect(result).to.be.ok;
+    });
+
+    it('should not flag as valid user a badformated one', function() {
+      // given
+      var validUser = new User(0, undefined, 'robert', 'on');
+      // when
+      var result = validUser.isValid();
+      // then
+      expect(result).to.be.not.ok;
+    });
+
+    it('should not flag as valid user an user with bad status ', function() {
+      // given
+      var validUser = new User(0, 12, 'robert', 'wtf');
+      // when
+      var result = validUser.isValid();
+      // then
+      expect(result).to.be.not.ok;
+    });
+  });
+});
 
 describe('usermodule', function() {
   describe('#isConnected', function() {
 
     it('should not flag as connected a not connected user', function() {
-      var usermodule = injectr('./lib/modules/usermodule.js', {})();
-
+      var instance = new UserModule();
       //given
       var id = 0;
-
       //when
-      var result = usermodule.isConnected(id);
-
+      var result = instance.isConnected(id);
       //then
-      expect(result).to.be.not.ok; 
+      expect(result).to.be.not.ok;
     });
 
     it('should flag as connected a connected user', function() {
+      var instance = new UserModule();
       //given 
-      var existingUser = new User(0,'JC','on');
-      var usermodule = injectr('./lib/modules/usermodule.js', {})();
-
+      var existingUser = new User(0,10,'JC','on');
+      instance.addUser(existingUser);
       //when
-      var result = usermodule.isConnected(existingUser.id); 
-
+      var result = instance.isConnected(existingUser.id);
       //then
       expect(result).to.be.ok;
     });
